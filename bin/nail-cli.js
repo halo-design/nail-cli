20fe2a92e1c612c4e25c0e4abd6b7718786cd517
OwlAford <244734834@qq.com>
2018-04-13 14:44:20 +0800

    Add New Files

---
+++ bin/nail-cli.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const chalk = require('chalk')
+const envPath = require('../lib/env-path')
+const configPath = envPath.projectResolve('nail.config.js')
+
+console.log(envPath.DIR.ROOT)
+console.log(envPath.DIR.PROJECT)
+
+console.log(envPath.resolve('lib/index.js'))
+console.log(process.argv.indexOf('dev') > -1)
+
+if (fs.existsSync(configPath)) {
+  const userConfig = require(configPath)
+  console.log(userConfig)
+} else {
+  console.log(chalk.red('The configuration file "nail.config.js" does not exist.'))
+}
