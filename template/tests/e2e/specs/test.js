5120d398b7b41de380c9d0368d88a28ffd0713f2
OwlAford <244734834@qq.com>
2018-04-25 17:28:33 +0800

    remove template

--- template/tests/e2e/specs/test.js
+++
@@ -1,9 +0,0 @@
-// https://docs.cypress.io/api/introduction/api.html
-
-describe('My First Test', () => {
-  it('Visits the Home Page', () => {
-    cy.visit('/')
-    cy.contains('h1', 'AWESOME NAIL!')
-  })
-})
-
--- template/tests/unit/Utils.spec.js
+++
@@ -1,5 +0,0 @@
-import { upcase } from '@/libs/utils.js'
-
-test('String converted to uppercase', () => {
-  expect(upcase('abc')).toBe('ABC')
-})
