7ab587ef5fdd58a91345bd6ed70e7c57e21ff51f
OwlAford <244734834@qq.com>
2018-07-30 10:19:13 +0800

    🔧 Fix some bugs

--- config/jest/transform/babel.js
+++ config/jest/transform/babel.js
@@ -3,9 +3,6 @@ const { createTransformer } = require('babel-jest');
 module.exports = createTransformer({
   presets: [
     require.resolve('@babel/preset-env'),
-    [require.resolve('@babel/preset-stage-2'), {
-      decoratorsLegacy: true,
-    }],
     require.resolve('@babel/preset-react'),
   ],
   babelrc: false,
